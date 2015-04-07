6b4c6e7f021bd433caa4f98d84aa210a23b8d7a7
pedroSilva4 <a64345@alunos.uminho.pt>
2015-04-07 17:56:56 +0100

    added challenge, music , player

---
+++ COMServer-Client/src/Server/Challenge.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+
+/**
+ *
+ * @author Pedro
+ */
+public class Challenge {
+    private class Question{
+        int question_n;
+        String question;
+        String[] answer  = new String[3];
+        int rightanswer;
+        byte[] music;
+        byte[] image;
+    }
+    String name;
+    int n_questions;
+    Map<Integer,Question> questions;
+    
+    
+    public Challenge(String filepath) throws IOException, JavaLayerException{
+       
+       InputStreamReader reader =  new FileReader(filepath);
+       BufferedReader buffer = new BufferedReader(reader);
+       String musicsDir = buffer.readLine().split("=")[1];
+       String imagesDir = buffer.readLine().split("=")[1];
+       this.n_questions = Integer.valueOf(buffer.readLine().split("=")[1]);
+       int nq = 1;
+        String musicPath = null;
+       for(int i = 3; i< n_questions+3;i++)
+       {
+            String[] parts = buffer.readLine().split("(\",\")|(\",)|(,\")");
+            
+            System.out.println("=====Questao "+nq+" :=======");
+             musicPath = "Challenges"+File.separator+musicsDir+File.separator+parts[0].split(",")[0];
+            String imagePath = imagesDir+File.separator+parts[0].split(",")[1];
+            
+            
+            System.out.println(musicPath);
+            System.out.println(imagePath);
+            System.out.println(parts[1]);
+            System.out.println(parts[2]);
+            System.out.println(parts[3]);
+            System.out.println(parts[4]);
+            System.out.println(parts[5]);
+            
+           
+            //new ByteArrayInputStream();
+            nq++;
+       }
+        
+        byte[] finalarr = Files.readAllBytes(Paths.get(musicPath));
+        ByteArrayInputStream input;
+        BufferedInputStream in ;
+        
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(finalarr);
+        byte[] arr = out.toByteArray();
+        System.out.print((float)finalarr.length/48000.f);
+        input = new ByteArrayInputStream(arr);
+        in = new BufferedInputStream(input);
+        Player mp3player = new Player(in);
+        
+        mp3player.play();
+        
+    }
+    
+}
---
+++ COMServer-Client/src/Server/teste1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Pedro
+ */
+public class teste1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        Challenge ch = new Challenge("Challenges"+File.separator+"desafio-000001.ch");
+    }
+    
+}
